1f07756664f0cf8f0b49c7777a5bf66e5d90c110
Adrian Cooney <cooney.adrian@gmail.com>
2015-06-25 10:13:15 +0100

    More skeletoning. Is that a verb? It must be a verb.

---
+++ src/Teamwork.js
@@ -0,0 +1,13 @@
+export default class Teamwork {
+    static parseTimestamp(timestamp) {
+
+    }
+
+    static parsePercent(percent) {
+
+    }
+
+    static parseTask(task) {
+        
+    }
+}
---
+++ src/TeamworkAPI.js
@@ -0,0 +1,17 @@
+export default class TeamworkAPI {
+    /**
+     * Make an unauthenticated request to the Teamwork API.
+     * @return {Promise} -> {Response}
+     */
+    static request(method, path, data) {
+
+    }
+
+    /**
+     * Make an authorized request to the Teamwork API.
+     * @return {Promise} -> {Response}
+     */
+    request() {
+
+    }
+}

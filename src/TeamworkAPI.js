a9b811f00276b884260be80c8e5dc132a1b869e8
Adrian Cooney <cooney.adrian@gmail.com>
2015-06-25 12:24:54 +0100

    Added login and instance-is-authorized logic.

--- src/TeamworkAPI.js
+++ src/TeamworkAPI.js
@@ -1,9 +1,18 @@
 import Promise from "bluebird";
 import { Debug } from "./library/Debug"
+import Teamwork from "./Teamwork";
 
 const debug = Debug("teamwork:api");
 
 export default class TeamworkAPI {
+    /**
+     * Create an authorized Teamwork API interface.
+     * @param  {String} auth The auth key.
+     */
+    constructor(auth) {
+        this.auth = auth;
+    }
+
     /**
      * Make an unauthenticated request to the Teamwork API.
      * @return {Promise} -> {Response}
@@ -23,6 +32,23 @@ export default class TeamworkAPI {
 
     }
 
+    /**
+     * Login to an installation with an email and password.
+     * @param  {String} email        Teamwork login email.
+     * @param  {String} password     Teamwork login password.
+     * @param  {String} installation Teamwork installation URL.
+     * @return {TeamworkAPI}              Authenticate Teamwork API.
+     */
+    static login(email, password, installation) {
+        installation = Teamwork.normalizeInstallationURL(installation);
+
+        TeamworkAPI.request("POST", `${installation}/launchpad/v1/login.json`, { 
+            email, password 
+        }).then((response) => {
+            return new TeamworkAPI(response.auth);
+        });
+    }
+
     static getAccounts(email, password) {
         return TeamworkAPI.request("POST", "http://authenticate.teamwork.com/account/search.json", {
             email, password
@@ -40,5 +66,12 @@ export default class TeamworkAPI {
                 installationUrl: "http://twtest.teamwork.com"
             }]
         });
+    }
+}
+
+export class LoginError extends Error {
+    constructor(email) { 
+        super(); 
+        this.message = `Login failed: Invalid credentials. (${email})`;
     }
 }

71c6785e432ce9a3ac376fa92c6473dbeeddcf4a
Adrian Cooney <cooney.adrian@gmail.com>
2015-06-25 11:28:16 +0100

    Tool is coming together now.
    * Inquirer converted to promises.
    * Validation scheme introduced.
    * Logging and failure scheme introduced.
    * Login command created and bare bones implemented.

--- src/Teamwork.js
+++ src/Teamwork.js
@@ -8,6 +8,32 @@ export default class Teamwork {
     }
 
     static parseTask(task) {
-        
+
+    }
+
+    /**
+     * Validate an email.
+     * @throws {ValidationError} If email is invalid.
+     * @param  {String} email Email to parse.
+     * @return {String}       Email.
+     */
+    static validateEmail(email) {
+        // TODO: Better email validation!
+        if(email.indexOf("@") === -1) {
+            throw new ValidationError(`Invalid email "${email}".`);
+        }
+
+        return email;
+    }
+
+    static parseInstallation(installation) {
+        return installation;
+    }
+}
+
+export class ValidationError extends Error {
+    constructor(reason) {
+        super();
+        this.message = "Validation Error: " + reason;
     }
 }
---
+++ src/TeamworkCLI.js
@@ -0,0 +1,71 @@
+import Promise from "bluebird";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import Teamwork from "./Teamwork";
+
+export default class TeamworkCLI {
+    /**
+     * Fail to execute and exit with a reason and code.
+     * @param  {String} reason The reason for the exit.
+     * @param  {Number} code   The exit code (default: 1)
+     */
+    static fail(reason, code = 1) {
+        TeamworkCLI.log(chalk.red("error"), reason);
+        process.exit(code);
+    }
+
+    /**
+     * Log output to the console.
+     * @param  {...*} logs Anything to pass to console.log.
+     */
+    static log(...logs) {
+        logs.unshift(chalk.blue("tw"));
+        console.log.apply(console, logs);
+    }
+
+    /**
+     * Validate some input and exit the program if it fails.
+     * @param  {Function}    validator The validator which throws ValidationError.
+     * @param  {...*} input     Input to pass to the
+     * @return {*}              Output from the validator.
+     */
+    static validateInput(validator, ...input) {
+        try {
+            return validator.apply(null, input);
+        } catch(err) {
+            TeamworkCLI.fail(err.message);
+        }
+    }
+
+    /**
+     * Inquirer validation requires either `true` returned
+     * for valid input or a {String} for invalid input which
+     * also serves as the error message. This function converts
+     * Teamwork validation scheme to Inquirer's scheme.
+     * @param  {Function} validator The validation function.
+     * @param  {...*} ..input   Input passed to the validator.
+     * @return {true|String}          True if valid, String as error message if invalid.
+     */
+    static inquirerValidateInput(validator, ...input) {
+        try {
+            validator.apply(null, input);
+            return true;
+        } catch(err) {
+            return err.message;
+        }
+    }
+
+    /**
+     * Promisfy Inquirer's prompt.
+     * @param  {Array} questions See Inquirer#prompt.
+     * @return {Promise} -> {Answers}
+     */
+    static prompt(questions) {
+        return new Promise((resolve, reject) => {
+            inquirer.prompt(questions, resolve);
+        });
+    }
+}
+
+// Export handy acces to Chalk
+TeamworkCLI.color = chalk;

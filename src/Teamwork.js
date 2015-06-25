1803eb4ee29a160008ec1a94b9b502d0f7aaffa2
Adrian Cooney <cooney.adrian@gmail.com>
2015-06-25 12:24:17 +0100

    Added installation URL normalization and parsing.

--- src/Teamwork.js
+++ src/Teamwork.js
@@ -12,6 +12,21 @@ export default class Teamwork {
     }
 
     /**
+     * Parse a teamwork installation URL.
+     * @param  {String} installation The installation URL.
+     * @return {Object}              { installation {String} }
+     */
+    static parseInstallation(installation) {
+        var match = installation.match(/^https?:\/\/(\w+)\.teamwork.com/);
+
+        if(!match) throw new ParserError(`Invalid installation URL: ${installation}`);
+
+        return {
+            installation: match[1]
+        }
+    }
+
+    /**
      * Validate an email.
      * @throws {ValidationError} If email is invalid.
      * @param  {String} email Email to parse.
@@ -26,8 +41,15 @@ export default class Teamwork {
         return email;
     }
 
-    static parseInstallation(installation) {
-        return installation;
+    /**
+     * Normalize an installation url to http://<installation>.teamwork.com
+     * @param  {String} installation The installation URL.
+     * @return {String}              http://<installation>.teamwork.com
+     */
+    static normalizeInstallationURL(installation) {
+        var parsed = Teamwork.parseInstallation(installation);
+
+        return `https://${parsed.installation}.teamwork.com`;
     }
 }
 
@@ -35,5 +57,12 @@ export class ValidationError extends Error {
     constructor(reason) {
         super();
         this.message = "Validation Error: " + reason;
+    }
+}
+
+export class ParserError extends Error {
+    constructor(reason) {
+        super();
+        this.message = "Parser Error: " + reason;
     }
 }
